e3c722d5856504f8962bc0ca9ced2f8f3d5a7f43
javanasse <julian.vanasse@gmail.com>
2023-06-07 15:04:02 -0400

    added setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='worldvocoder',
     version='0.0.1',
-    packages=find_packages(include=['worldvocoder', 'worldvocoder.*'])
+    packages=find_packages(include=['worldvocoder', 'worldvocoder.*']),
+    download_url="https://github.com/javanasse/Python-WORLD/archive/refs/tags/v0.tar.gz"
 )
